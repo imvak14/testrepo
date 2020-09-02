15ba8a569b1c5fdf7463e6b61c7653b9f37c4704
imvak14 <64520304+imvak14@users.noreply.github.com>
2020-09-02 16:00:24 +0530

    Add files via upload

--- test.tf
+++ test.tf
@@ -8,14 +8,3 @@ terraform {
   }
 }
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-  }
-}
-
-provider "kubernetes" {
-  host = "https://0.0.0.0"
-}
